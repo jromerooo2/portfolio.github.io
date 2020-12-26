16356e8892c2f513bc719cbc5f2453213e5afaf2
jromerooo2 <75763058+jromerooo2@users.noreply.github.com>
2020-12-26 13:31:35 -0600

    added new files

---
+++ main.js
@@ -0,0 +1,20 @@
+function langdetected(lang){
+    if (lang == 0) {
+        //console.log("english")
+        location.href = "main.html"
+    }
+    if (lang == 1) {
+        //console.log("spanish")
+        location.href = "mainES.html"
+    }
+    if (lang == 2) {
+        //console.log("Francés")
+        location.href = "mainFR.html"
+    }
+}
+
+function links(){
+
+        location.href ="www.aranadentistry.com"
+
+}
